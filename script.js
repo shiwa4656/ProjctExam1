754c22b54d0cbfc1e90ed1ec138e59f5b2a5cc46
shirwac <shirwacstudy@gmail.com>
2024-05-15 01:11:31 +0200

    wednesdayCommit

--- script.js
+++ script.js
@@ -272,3 +272,14 @@ function signOut() {
   localStorage.removeItem("token");
   location.reload();
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const menuItems = document.querySelector('.menu-items');
+  
+  menuToggle.addEventListener('click', function () {
+      menuItems.classList.toggle('active');
+  });
+});
+
+
